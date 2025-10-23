b35655e96f7cf691a0fa51ba77ed3b3b14e49716
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-23 13:31:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // complete this js code
-function Person(name, age) {}
+class Person {
+	constructor(name,age){
+	this.name=name;
+	this.age=age;
+	}
+	greet:function(){
+		console.log(`Hello,my name is ${this.name}, I am ${this.age} years old.`);
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+class Employee extends Person {
+	constructor(name,age,jobTitle){
+		super(name,age);
+		this.jobTitle=jobTitle;
+	}
+	jobGreet:function(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+		}
+}
 
 // Do not change code below this line
 window.Person = Person;
